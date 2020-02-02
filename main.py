eb2da5b9b42ccda997109a135b7c4cb17f5e2035
unknown <kenrickng90@gmail.com>
2020-02-02 12:35:28 +0800

    fist commit, too slow

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the arrayManipulation function below.
+def arrayManipulation(n, queries):
+    arr = [0]*n
+    for i in range(len(queries)):
+        for k in range(queries[i][0]-1,queries[i][1]):
+            arr[k]+=queries[i][2]
+    return max(arr)
+
+if __name__ == '__main__':
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+    queries = []
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+    result = arrayManipulation(n, queries)
+    print(result)
