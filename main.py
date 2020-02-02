4ed83fc056c04839b3128a3eebf2c341c6299efd
unknown <kenrickng90@gmail.com>
2020-02-02 13:58:03 +0800

    faster commit

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ import sys
 
 # Complete the arrayManipulation function below.
 def arrayManipulation(n, queries):
-    arr = [0]*n
+    difffromarr = [0]*(n+1)
+    maxarr = [0]*n
     for i in range(len(queries)):
-        for k in range(queries[i][0]-1,queries[i][1]):
-            arr[k]+=queries[i][2]
-    return max(arr)
+        difffromarr[queries[i][0]-1] += queries[i][2]
+        difffromarr[queries[i][1]] -= queries[i][2]
+    for i in range(n):
+        if i ==0:
+            maxarr[0] = difffromarr[0]
+            continue
+        maxarr[i]=maxarr[i-1]+difffromarr[i]
+    # print(maxarr)
+    return max(maxarr)
 
 if __name__ == '__main__':
     nm = input().split()
